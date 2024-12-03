c5fe7c57bf558b6e396c67dc129e5484c9d8a579
Zach <zraisnorman@gmail.com>
2024-12-03 10:31:24 -0500

    finish 3-1

--- day3-1.py
+++ day3-1.py
@@ -1,2 +1,13 @@
 import re
 
+total = 0
+
+with open("day3data.txt", 'r') as f:
+  search = re.findall(r'mul\(\d{1,3},\d{1,3}\)', f.read())
+  for s in search:
+    s = s.replace('mul(', '').replace(')', '')
+    s = s.split(',')
+    val = int(s[0]) * int(s[1])
+    total += val
+
+print(total)
