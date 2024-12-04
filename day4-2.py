e44fafa7b4fe0638a62e6da7f974fce579f44d13
Zach <zraisnorman@gmail.com>
2024-12-04 10:56:07 -0500

    4-2 done

---
+++ day4-2.py
@@ -0,0 +1,53 @@
+p = []
+
+with open("day4data.txt", 'r') as f:
+    data = f.read().splitlines()
+    for line in data:
+      row = []
+      for c in line:
+        row.append(c)
+      p.append(row)
+
+w = len(p[0])
+h = len(p)
+
+# reverse the x and y of the matrix to fit x, y coordinates
+p = list(map(list, zip(*p)))
+
+def check_bounds(x, y):
+  if x < 0 or x >= w:
+    return False
+  if y < 0 or y >= h:
+    return False
+  return True
+
+def check_space(x, y):
+  if x > 0 and x < w - 1 and y > 0 and y < h - 1:
+    return True
+
+# ugh
+def check_MAS(x, y):
+  for i in [-1, 1]:
+    for j in [-1, 1]:
+      if i == 0 and j == 0:
+        continue
+      if p[x + i][y + j] == "M":
+        if p[x - i][y - j] == "S":
+          if p[x + i][y - j] == "M":
+            if p[x - i][y + j] == "S":
+              return True
+          if p[x - i][y + j] == "M":
+            if p[x + i][y - j] == "S":
+              return True
+  return False
+
+def search():
+  count = 0
+  for i in range(w):
+    for j in range(h):
+      if p[i][j] == "A" and check_space(i, j):
+        if check_MAS(i, j):
+          count = count + 1
+  print(count)
+
+search()
